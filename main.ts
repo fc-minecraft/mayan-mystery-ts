6f8b1f3f9494c58f296a0daf38d5e4054bf308a2
neonerz <gerald@rewrite.media>
2025-01-14 11:01:40 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,39 @@
+//ENUMS
 
+enum Three_Axis {
+    //% block = "forward"
+    Forward,
+    //% block = "left"
+    Left,
+    //% block = "right"
+    Right
+}
+
+//GLOBAL VARIABLES
+const communicationsTimeout = 75;
+
+//%  block="Mayan Mystery" weight=200 color=#0B3D91 icon="\uf186"
+namespace mayanMystery {
+
+/**
+ * This function moves the Agent
+ * in three directions
+ */
+//% block = "agent move %d by %n"
+    export function agent_move(d: Three_Axis, n: number): void {
+        for (let i =0; i < n; i++){
+            switch (d) {
+                case Three_Axis.Forward:
+                    player.execute("scoreboard players set .output global 1")
+                    break;
+                case Three_Axis.Left:
+                    player.execute("scoreboard players set .output global 2")
+                    break;
+                case Three_Axis.Right:
+                    player.execute("scoreboard players set .output global 3")
+            }
+            loops.pause(communicationsTimeout)
+        }
+    }
+
+}
